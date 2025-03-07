518906952adea03a30bec766e6fcaf6ce2f230fa
Yihan Wang <wyh20021207@gamil.com>
2025-03-07 10:54:22 +0800

    terminal mode下jk退出edit mode只在toggle terminal下生效（修改yazi使用j键滚动慢的问题）

--- lua/plugins/ui/terminal.lua
+++ lua/plugins/ui/terminal.lua
@@ -39,13 +39,19 @@ return {
 		},
 		close_on_exit = true,
 		auto_scroll = true, -- automatically scroll to the bottom on terminal output
+		on_open = function(term)
+			-- yazi 打开的窗口也是终端模式，yazi 中按下 jk 也会进入visual mode
+			-- 而且为了确认按键，yazi 下不能连续按 j 键，也就会导致 下移目录非常慢
+			-- 仅在 toggleterm 窗口设置 "jk" 映射
+			vim.api.nvim_buf_set_keymap(term.bufnr, "t", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
+		end,
 	},
 	keys = {
-		{ "jk", [[<C-\><C-n>]], mode = "t", { buffer = 0 } },
-		{ "<C-h>", [[<Cmd>wincmd h<CR>]], mode = "t", { buffer = 0 } },
-		{ "<C-j>", [[<Cmd>wincmd j<CR>]], mode = "t", { buffer = 0 } },
-		{ "<C-k>", [[<Cmd>wincmd k<CR>]], mode = "t", { buffer = 0 } },
-		{ "<C-l>", [[<Cmd>wincmd l<CR>]], mode = "t", { buffer = 0 } },
-		{ "<C-w>", [[<C-\><C-n><C-w>]], mode = "t", { buffer = 0 } },
+		-- 通过 ctrl + hjkl 切换窗口，主要使用在 direction != 'float' mode 时，从terminal切换到文本编辑窗口，使用 float mode 的话，这些 keymap 副作用
+		-- { "<C-h>", [[<Cmd>wincmd h<CR>]], mode = "t", { buffer = 0 } },
+		-- { "<C-j>", [[<Cmd>wincmd j<CR>]], mode = "t", { buffer = 0 } },
+		-- { "<C-k>", [[<Cmd>wincmd k<CR>]], mode = "t", { buffer = 0 } },
+		-- { "<C-l>", [[<Cmd>wincmd l<CR>]], mode = "t", { buffer = 0 } },
+		-- { "<C-w>", [[<C-\><C-n><C-w>]], mode = "t", { buffer = 0 } },
 	},
 }
