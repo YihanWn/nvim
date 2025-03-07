dc38a47f5bb4c3dcc3540056d951c806ac3d7c76
Yihan Wang <wyh20021207@gamil.com>
2025-03-07 11:35:13 +0800

    remove Glow(markdown preview plugins)

--- lua/plugins/utils/markdown.lua
+++ lua/plugins/utils/markdown.lua
@@ -1,11 +1,20 @@
 -- NOTE: preview markdown
 return {
 
-	{ -- glow: use ":Glow" to preview Markdown
-		"ellisonleao/glow.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
+	-- 打开比较慢，而且无法打开大文件
+	-- { -- glow: use ":Glow" to preview Markdown
+	-- 	"ellisonleao/glow.nvim",
+	-- 	-- event = "VeryLazy",
+	-- 	config = {
+	-- 		border = "curve", -- floating window border config
+	-- 		-- style = "dark", -- filled automatically with your current editor background, you can override using glow json style
+	-- 		pager = false,
+	-- 		width = 80,
+	-- 		height = 100,
+	-- 		width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
+	-- 		height_ratio = 0.7,
+	-- 	},
+	-- },
 
 	{ -- markdown preview
 		"OXY2DEV/markview.nvim",
