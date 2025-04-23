52d444a2c4e4d11c968124c661bc982356d847e8
Yihan Wang <wyh20021207@gamil.com>
2025-04-23 11:20:01 +0800

    [snacks] picker auto hide explorer "input"

--- lua/plugins/utils/snacks_config/picker.lua
+++ lua/plugins/utils/snacks_config/picker.lua
@@ -1,4 +1,4 @@
----@class snacks.picker.Config
+---@class snacks.picker.Confi
 ---@field multi? (string|snacks.picker.Config)[]
 ---@field source? string source name and config to use
 ---@field pattern? string|fun(picker:snacks.Picker):string pattern used to filter items by the matcher
@@ -46,7 +46,74 @@
 ---@field debug? snacks.picker.debug|{}
 return {
   prompt = " ",
-  sources = {},
+  sources = {
+    explorer = {
+      finder = "explorer",
+      sort = { fields = { "sort" } },
+      supports_live = true,
+      tree = true,
+      watch = true,
+      diagnostics = true,
+      diagnostics_open = false,
+      git_status = true,
+      git_status_open = false,
+      git_untracked = true,
+      follow_file = true,
+      focus = "list",
+      auto_close = false,
+      jump = { close = false },
+      layout = {
+        preset = "sidebar",
+        preview = false,
+        auto_hide = { "input" },
+      },
+      -- to show the explorer to the right, add the below to
+      -- your config under `opts.picker.sources.explorer`
+      -- layout = { layout = { position = "right" } },
+      formatters = {
+        file = { filename_only = true },
+        severity = { pos = "right" },
+      },
+      matcher = { sort_empty = false, fuzzy = false },
+      config = function(opts)
+        return require("snacks.picker.source.explorer").setup(opts)
+      end,
+      win = {
+        list = {
+          keys = {
+            ["<BS>"] = "explorer_up",
+            ["l"] = "confirm",
+            ["h"] = "explorer_close", -- close directory
+            ["a"] = "explorer_add",
+            ["d"] = "explorer_del",
+            ["r"] = "explorer_rename",
+            ["c"] = "explorer_copy",
+            ["m"] = "explorer_move",
+            ["o"] = "explorer_open", -- open with system application
+            ["P"] = "toggle_preview",
+            ["y"] = { "explorer_yank", mode = { "n", "x" } },
+            ["p"] = "explorer_paste",
+            ["u"] = "explorer_update",
+            ["<c-c>"] = "tcd",
+            ["<leader>/"] = "picker_grep",
+            ["<c-t>"] = "terminal",
+            ["."] = "explorer_focus",
+            ["I"] = "toggle_ignored",
+            ["H"] = "toggle_hidden",
+            ["Z"] = "explorer_close_all",
+            ["]g"] = "explorer_git_next",
+            ["[g"] = "explorer_git_prev",
+            ["]d"] = "explorer_diagnostic_next",
+            ["[d"] = "explorer_diagnostic_prev",
+            ["]w"] = "explorer_warn_next",
+            ["[w"] = "explorer_warn_prev",
+            ["]e"] = "explorer_error_next",
+            ["[e"] = "explorer_error_prev",
+          },
+        },
+      },
+    },
+  },
   focus = "input",
   layout = {
     cycle = true,
